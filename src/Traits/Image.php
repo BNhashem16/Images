a2102ef04539915fd60d747a5134d036ddbb5726
Ahmed Hashem <a.hashem@thetailorsdev.com>
2020-12-27 23:02:21 +0200

    first commit

--- src/Traits/Image.php
+++ src/Traits/Image.php
@@ -1,20 +1,40 @@
 <?php
 namespace Hashem\Image\Traits;
 
+use Illuminate\Support\Facades\Storage;
 
 /**
  *
  */
 trait Image
 {
+    # Store Images.
     public static function store($folder , $parameter)
     {
-        if ($parameter) {
+        if (request()->$parameter !== null) {
             $image = request()->file($parameter)->store($folder.'/'.$parameter, 'public');
         } else {
             $image = null;
         }
         return $image;
+    }
+
+    # Update Images.
+    public static function update($folder , $parameter , $model)
+    {
+        if (request()->$parameter !== null) {
+            Storage::disk('public')->delete($model->$parameter);
+            $image = request()->file($parameter)->store($folder.'/'.$parameter, 'public');
+        } else {
+            $image = $model->$parameter;
+        }
+        return $image;
+    }
+
+    # Destroy Images.
+    public static function destroy($parameter , $model)
+    {
+        Storage::disk('public')->delete($model->$parameter);
     }
 }
 
